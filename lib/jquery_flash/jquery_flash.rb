eb4c6365ebe977df9b87a47cb7116e0aaf4b2ff7
Krishna Shilpakar <krishna@sprout-technology.com>
2010-12-03 14:35:22 +0545

    refactored code

--- lib/jquery_flash/jquery_flash.rb
+++ lib/jquery_flash/jquery_flash.rb
@@ -2,14 +2,17 @@ module JqueryFlash
   
     def jquery_flash(options={})
       output = ""
-      defaults = { :stay => true, :script_tag => true, :msg => '' }.merge(options)
-      [:error, :warning, :info, :notice].each do |key|
-        msg = flash[key] || defaults[:msg]
-        if msg
-          output << display_flash(key, msg, defaults)
-        end
+      defaults = { :stay => true}.merge(options)
+      if defaults[:message]
+        output << display_flash(:notice, defaults[:message], defaults)
+      else
+         [:error, :warning, :info, :notice].each do |key|
+            if flash[key]
+              output << display_flash(key, flash[key], defaults)
+            end
+          end
       end
-      output
+     output
     end
   
     def display_flash(type, text, options={})
@@ -19,7 +22,7 @@ module JqueryFlash
     end
   
       def jquery_flash_script(options={})
-        return content_tag(:script, jquery_flash(options), nil, false) if options[:script_tag].present?
-        jquery_flash(options)
+        return jquery_flash(options) if options[:script_tag] == false
+        return content_tag(:script, jquery_flash(options), nil, false)
     end
 end
